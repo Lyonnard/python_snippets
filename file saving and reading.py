f431e598edb96667049adfacffbb366a3ef8eea6
Leonardo Del Bino <leonardo.del-bino@mpl.mpg.de>
2020-12-09 12:41:02 +0100

    Starting repo

---
+++ file saving and reading.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  4 17:12:59 2020
+
+@author: ldelbin
+"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+header = 'ciao \n io sono un header \n e contengo, virgole'
+array = [[1, 2, 3, 4, 5, 6, 7, 8, 9], [11, 22, 33, 44, 55, 66, 77, 88, 99]]
+a = np.array(array)
+a = a.T
+df = pd.DataFrame(data=a, columns=('x', 'y'))
+df.to_csv('test.csv', index=False, header=header)
+
+np.savetxt('test2.csv', a, delimiter=',', header=header)
+
+a_l = np.loadtxt('test2.csv', delimiter=',')
+
+a = np.array([1, 2, 3, 4])
+b = np.array([5, 6, 7, 8])
+c = np.c_[a, b]
+d=7
