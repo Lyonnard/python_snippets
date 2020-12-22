138746eec6b3d09da7f0b560915a73306ab47d71
Leonardo Del Bino <leonardo.del-bino@mpl.mpg.de>
2020-12-22 18:07:38 +0100

    Started docstring and plotting

---
+++ class docstring template.py
@@ -0,0 +1,4 @@
+class Person:
+    def __init__(self, firstname, age):
+        self.age = age
+        self.firstname = firstname
---
+++ plotting.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+plt.close("all")
+
+
+fig1 = plt.figure()
+fig1, ax_lst = plt.subplots(
+    2, 2
+)  # creates a 2 by 2 figure list, if empty create 1 axes
+# axes is a single panel
+# axis is each direction in the axes
+# artist is every visible item
+
+
+def my_plotter(ax, data1, data2, param_dict):
+    out = ax.plot(data1, data2, **param_dict)
+    return out
+
+
+data1, data2, data3, data4 = np.random.randn(4, 100)
+x = np.arange(100)
+fig, ax = plt.subplots(1, 1)
+
+# to get the xkcd style
+# use in context manager since it changes many rcparams
+with plt.xkcd():
+    my_plotter(ax, x, data2, {"marker": "x"})
+plt.tight_layout()
