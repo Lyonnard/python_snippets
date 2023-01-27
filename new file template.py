5c8a4ea86f2a2b81bd23411befa3f528460e7ac4
Leonardo <lyonnard@gmail.com>
2023-01-27 18:04:01 +0100

    added to new file template

--- new file template.py
+++ new file template.py
@@ -27,14 +27,19 @@ if __name__ == "__main__":
     # Create an instance of the class and call the method
     my_instance = MyClass("example_attribute")
     my_instance.my_method()
+
+    # For loop using enumerate example
+    my_list = ["item1", "item2", "item3"]
+    for i, item in enumerate(my_list):
+        print(f"Index: {i}, Item: {item}")
     
     # Plot example
     x = np.linspace(0, 2*np.pi, 100)
     y = np.sin(x)
-    ax.plot(x, y)
+    ln = ax.plot(x, y) #also save it as a line, just in case
 
     plt.tight_layout()
-    fig.savefig('Koen bias.png', dpi=600, bbox_inches='tight', pad_inches=0)
-    #fig.savefig('Koen bias.pdf', dpi=600, bbox_inches='tight', pad_inches=0)
+    fig.savefig('plot.png', dpi=600, bbox_inches='tight', pad_inches=0)
+    #fig.savefig('plot.pdf', dpi=600, bbox_inches='tight', pad_inches=0)
 
     plt.show()
